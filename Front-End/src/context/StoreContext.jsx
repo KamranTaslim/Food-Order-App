21f960034e718813c13f8126021924b004d755d3
Md Kamran Taslim <54260472+KamranTaslim@users.noreply.github.com>
2024-07-30 12:53:52 +0530

    Update StoreContext.jsx

--- Front-End/src/context/StoreContext.jsx
+++ Front-End/src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://food-order-app-backend-hlua.onrender.com";
   const [token, setToken] = useState("");
   //show data from database
   const [food_list, setFood_list] = useState([]);
