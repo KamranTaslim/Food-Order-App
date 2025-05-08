01c6b74709d0d71bcd12eec028a15f32c0cac5e0
Md Kamran Taslim <54260472+KamranTaslim@users.noreply.github.com>
2025-05-08 23:14:52 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -8,7 +8,7 @@ import Orders from "./pages/Orders/Orders";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 const App = () => {
-  const url = "https://food-order-app-backend-hlua.onrender.com";
+ const url = "https://foodorderapp.live/";
   return (
     <div>
       <ToastContainer />
